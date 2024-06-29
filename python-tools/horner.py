914399f55fd26a4041701afbcb7b5cebcf39f92c
Jan Dreske <jandreske@web.de>
2024-06-30 01:20:20 +0200

    updated horner to better reflect scaling

--- python-tools/horner.py
+++ python-tools/horner.py
@@ -1,9 +1,9 @@
 
 def calc(a, e, s):
     if e == 1:
-        return s*a
-    return calc(a, e-2, 1 - (s*a*a)/(10**18*e*(e-1)))
+        return s*a/10**9
+    return calc(a, e-2, 10**9 - (s*a*a)/(10**18*e*(e-1)))
 
 
-print(calc(1047197551, 11, 1))
-print(calc(-1168378317, 11, 1))
+print(calc(1047197551, 11, 10**9))
+print(calc(-1168378317, 11, 10**9))
